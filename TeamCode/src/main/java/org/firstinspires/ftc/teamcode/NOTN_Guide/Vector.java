15d24193b477d15b4e1e4517c35c5b39419d5c34
VeeAyeInIn <veeayeinin@gmail.com>
2018-11-14 16:53:04 -0500

    Update Vector.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTN_Guide/Vector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTN_Guide/Vector.java
@@ -1,17 +1,34 @@
 package org.firstinspires.ftc.teamcode.NOTN_Guide;
 
+/**
+ * Made this for the hell of it. I was bored.
+ * For Robotics NOTN_Guide I guess.
+ *
+ * @author Connor Vann
+ */
 public class Vector {
 
+  // 3D Coordinates (Z can be whatever if you don't want 3D)
   private double x;
   private double y;
   private double z;
 
+  /**
+   * Sets up the initial values of the Vector
+   *
+   * @param x The initial x coordinate
+   * @param y The initial y coordinate
+   * @param z The initial z coordinate
+   */
   public Vector(double x, double y, double z) {
     this.x = x;
     this.y = y;
     this.z = z;
   }
 
+  /**
+   * Sets up the vector with the initial values at 0, 0, 0
+   */
   public Vector() {
     x = 0;
     y = 0;
@@ -42,6 +59,13 @@ public class Vector {
     this.z = z;
   }
 
+  /**
+   * Shift the vector along the respective axis
+   *
+   * @param shiftX How far along the x-axis to move
+   * @param shiftY How far along the y-axis to move
+   * @param shiftZ How far along the z-axis to move
+   */
   public void translate(double shiftX, double shiftY, double shiftZ) {
     x += shiftX;
     y += shiftY;
