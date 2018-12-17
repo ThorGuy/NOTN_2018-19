505cc48382805fecd8e1086ad9346d7a8e958250
Sami <samimac03@gmail.com>
2018-12-17 17:13:02 -0500

    Did stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTN_Guide/MyTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTN_Guide/MyTeleop.java
@@ -1,59 +1,155 @@
 package org.firstinspires.ftc.teamcode.NOTN_Guide;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;   //Allows us to use the library
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-public class MyTeleop extends OpMode {
 
-    private DcMotor leftFront, rightFront, leftBack, rightBack, ArmRotate, ArmLift;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
-    public void init(){
-        //Initialization Code
-
-        //Assigning the motor variables to the actual motors
-        //servoone = hardwareMap.
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        rightFront = hardwareMap.dcMotor.get("rightFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        rightBack = hardwareMap.dcMotor.get("rightBack");
-        ArmRotate = hardwareMap.dcMotor.get("ArmRotate");
-        ArmLift = hardwareMap.dcMotor.get("ArmLift");
-
-}
-public void loop(){
-    float lrmovement = gamepad1.left_stick_x;
-    float fbmovement = gamepad1.left_stick_y;
-    float rotation = gamepad1.right_stick_x;
-    double [] vecsetone = new double[4];
-    vecsetone[1] = fbmovement;
-    vecsetone[2] = fbmovement;
-    vecsetone[3] = fbmovement;
-    vecsetone[4] = fbmovement;
-    double [] vecsettwo = new double[4];
-    vecsettwo[1] = lrmovement;
-    vecsettwo[2] = lrmovement*-1;
-    vecsettwo[3] = lrmovement*-1;
-    vecsettwo[4] = lrmovement;
-    double [] vecsetthree = new double[4];
-    vecsetthree[1] = rotation;
-    vecsetthree[2] = rotation*-1;
-    vecsetthree[3] = rotation;
-    vecsetthree[4] = rotation*-1;
-    double [] finalvecs = new double[4];
-    finalvecs[1] = (vecsetone[1]+vecsettwo[1]+vecsetthree[1])/3;
-    finalvecs[2] = (vecsetone[2]+vecsettwo[2]+vecsetthree[2])/3;
-    finalvecs[3] = (vecsetone[3]+vecsettwo[3]+vecsetthree[3])/3;
-    finalvecs[4] = (vecsetone[4]+vecsettwo[4]+vecsetthree[4])/3;
-
-    leftFront.setPower(finalvecs[1]);
-    rightFront.setPower(finalvecs[2]);
-    leftBack.setPower(finalvecs[3]);
-    rightBack.setPower(finalvecs[4]);
-    ArmLift.setPower(gamepad2.left_stick_y);
-    ArmRotate.setPower(gamepad2.right_stick_y);
+import java.util.List;
 
 
+@Autonomous(name="AutoNomus", group="Linear Opmode")
+public class AutoOp extends LinearOpMode {
+
+    DcMotor leftFront, leftBack, rightFront, rightBack, armLift, armRotate;
+        private static final String TFOD_MODEL_ASSET = "RoverRuckus.tflite";
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private static final String LABEL_SILVER_MINERAL = "Silver Mineral";
+
+
+
+    private static final String VUFORIA_KEY = "AQYNN0//////AAABmeBGDwd4s0UkgGceRPKE4yeCzY2Nkmj7J15evwERwC16TDzbe1BbRpYNU3wMJJ5473aJgTzyjs/1eeI9Nq8EoXEN6lQVCO+04d0yUK2eKYEqlIC6+RXUQjgZDBV1wiBUOtMgD9qiQpmbrq17lRneXhDuWsfRR9iA7GGI4XhTINNRK5IV2d6242wnZLl913NPsb/yiwd4ltXvq2ZFIq4RXzgMgM8bpFuTHfe8tEWYguG6R7lRZ5W8IyJTe9RmXjcIeuROCz/32jWelgd+6p3ubE2JzquKplm7VC7XkLsnrHX5OaUHB/3IhtPGr/troy0vvqNJmigSL9V8fxVO4b/psyT6WCbhdLMjpsCWbnrJQ3Re";
+
+    private VuforiaLocalizer vuforia;
+
+    private TFObjectDetector tfod;
+    @Override
+    public void runOpMode(){
+        getVision();
 
     }
+    public void getVision() {
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+        armLift = hardwareMap.dcMotor.get("ArmLift");
+        armRotate = hardwareMap.dcMotor.get("ArmRotate");
 
+        initVuforia();
+
+        if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
+            initTfod();
+        } else {
+            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
+        }
+
+        telemetry.update();
+        waitForStart();
+
+        if (opModeIsActive()) {
+            /** Activate Tensor Flow Object Detection. */
+            if (tfod != null) {
+                tfod.activate();
+            }
+
+            while (opModeIsActive()) {
+                if (tfod != null) {
+
+                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                    if (updatedRecognitions != null) {
+                        telemetry.addData("# Object Detected", updatedRecognitions.size());
+                        if (updatedRecognitions.size() == 3) {
+                            int goldMineralX = -1;
+                            int silverMineral1X = -1;
+                            int silverMineral2X = -1;
+                            for (Recognition recognition : updatedRecognitions) {
+                                if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                                    goldMineralX = (int) recognition.getLeft();
+                                } else if (silverMineral1X == -1) {
+                                    silverMineral1X = (int) recognition.getLeft();
+                                } else {
+                                    silverMineral2X = (int) recognition.getLeft();
+                                }
+                            }
+                            if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                                if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                                    telemetry.addData("Gold Mineral Position", "Left");
+                                } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                                    telemetry.addData("Gold Mineral Position", "Right");
+                                } else {
+                                    telemetry.addData("Gold Mineral Position", "Center");
+                                }
+                            }
+                        }
+                        telemetry.update();
+                    }
+                }
+            }
+        }
+
+        if (tfod != null) {
+            tfod.shutdown();
+        }
+    }
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the Tensor Flow Object Detection engine.
+    }
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_GOLD_MINERAL, LABEL_SILVER_MINERAL);
+    }
+    public void move(int[] power, int[] location){
+        //rotate
+        leftBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFront.setTargetPosition(location[0]);
+        leftFront.setPower(power[0]);
+        leftBack.setTargetPosition(location[1]);
+        leftBack.setPower(power[1]);
+        rightFront.setTargetPosition(location[2]);
+        rightFront.setPower(power[2]);
+        rightFront.setTargetPosition(location[3]);
+        rightFront.setPower(power[3]);
+    }
+    public void drive(int degree){
+        int rotation = 360;
+        int[] location = new int[] {rotation * degree, rotation * degree, rotation * degree, rotation * degree}; //set zero to 360 degree rotation
+        int[] power = new int[]{1, 1, 1, 1};
+        move(power,location);
+    }
+
+    public void armMove(int power, int location){
+        //up down
+        armRotate.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotate.setTargetPosition(location);
+        armRotate.setPower(power);
+    }
+    public void rotateArm(int power, int location){
+        //rotate arm
+        armLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armLift.setTargetPosition(location);
+        armLift.setPower(power);
+
+    }
 }
