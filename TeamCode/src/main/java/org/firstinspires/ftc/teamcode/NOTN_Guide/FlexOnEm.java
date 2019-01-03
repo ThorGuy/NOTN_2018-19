0ce99876af2e8848dfd25df5ac5cf43436f610c6
VeeAyeInIn <veeayeinin@gmail.com>
2019-01-03 16:17:55 -0500

    Update FlexOnEm.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTN_Guide/FlexOnEm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NOTN_Guide/FlexOnEm.java
@@ -5,28 +5,32 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name="FlexOnEm");
+@Autonomous(name="FlexOnEm"); // Name of the Task
 public class FlexOnEm extends OpMode {
+
+    // The physical components of the robot
     private DcMotor leftFront, rightFront, leftBack, rightBack, ArmRotate, ArmLift;
     private Servo item, dropper, sweeper, rotate;
 
+    // Initialization period, what happens at the start
     public void init() {
-        leftFront = hardwareMap.dcMotor.get("leftFront");
-        rightFront = hardwareMap.dcMotor.get("rightFront");
-        leftBack = hardwareMap.dcMotor.get("leftBack");
-        rightBack = hardwareMap.dcMotor.get("rightBack");
-        ArmRotate = hardwareMap.dcMotor.get("ArmRotate");
-        ArmLift = hardwareMap.dcMotor.get("ArmLift");
 
-        item = hardwareMap.servo.get("item");
-        rotate = hardwareMap.servo.get("rotate");
-        sweeper = hardwareMap.servo.get("sweeper");
-        dropper = hardwareMap.servo.get("dropper");
+        leftFront   =  hardwareMap.dcMotor.get("leftFront");
+        rightFront  =  hardwareMap.dcMotor.get("rightFront");
+        leftBack    =  hardwareMap.dcMotor.get("leftBack");
+        rightBack   =  hardwareMap.dcMotor.get("rightBack");
+        ArmRotate   =  hardwareMap.dcMotor.get("ArmRotate");
+        ArmLift     =  hardwareMap.dcMotor.get("ArmLift");
 
-        public void loop(){
-            int countperrotation = 1120;
+        item        =  hardwareMap.servo.get("item");
+        rotate      =  hardwareMap.servo.get("rotate");
+        sweeper     =  hardwareMap.servo.get("sweeper");
+        dropper     =  hardwareMap.servo.get("dropper");
 
-        }
+    }
 
+    // Repeat this
+    public void loop() {
+        int countperrotation = 1120;
     }
 }
