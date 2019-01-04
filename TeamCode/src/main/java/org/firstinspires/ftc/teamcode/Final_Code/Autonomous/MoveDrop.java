96c9c22c4d42fce1d38a041def9edb7f593e2cb5
piratedk727 <32278912+piratedk727@users.noreply.github.com>
2019-01-03 19:17:17 -0500

    made a fail-safe and added a return statement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Final_Code/Autonomous/MoveDrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Final_Code/Autonomous/MoveDrop.java
@@ -5,17 +5,15 @@ public class MoveDrop{
 		if(goldPos.equals("Left")){
 			autoUtil.moveDirection("clockwise",0.25,posOneRot);
 			autoUtil.moveDirection("forward",0.25,posOneMove);
-			return true;
 		}else if(goldPos.equals("Center")){
 			autoUtil.moveDirection("clockwise",0.25,posTwoRot);
 			autoUtil.moveDirection("forward",0.25,posTwoMove);
-			return true;
 		}else if(goldPos.equals("Right")){
 			autoUtil.moveDirection("clockwise",0.25,posThreeRot);
 			autoUtil.moveDirection("forward",0.25,posThreeMove);
-			return true;
 		}else{
 			return false;
 		}
+		return true;
 	}
 }
